50211c80da6597ffaf0c0711bd37a0e0b3309e99
akshay <akshay.kapoor@paxcel.net>
2014-11-11 12:38:38 +0530

    Added no arg constructor to PuppetModuleMapping

--- src/main/java/com/boxupp/db/beans/PuppetModuleMapping.java
+++ src/main/java/com/boxupp/db/beans/PuppetModuleMapping.java
@@ -14,6 +14,10 @@ public class PuppetModuleMapping {
 		this.puppetModule = puppetModule;
 		this.project = project;
 	}
+	
+	public PuppetModuleMapping(){
+		
+	}
 
 	@DatabaseField(canBeNull = false, generatedId = true, useGetSet = true)
 	private Integer ID;
