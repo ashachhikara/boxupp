71526164fcbde69f37d64223ea3ce25c031f7d4a
akshay <akshay.kapoor@paxcel.net>
2014-12-09 17:00:47 +0530

    Added license information in all code files and added the LICENSE file

--- src/main/java/com/boxupp/db/DerbyConfig.java
+++ src/main/java/com/boxupp/db/DerbyConfig.java
@@ -1,13 +1,40 @@
+/*******************************************************************************
+ *  Copyright 2014 Paxcel Technologies
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *******************************************************************************/
 package com.boxupp.db;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.boxupp.utilities.OSProperties;
+
 public class DerbyConfig {
 	
 //	public static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
-	public static final String JDBC_URL = "jdbc:derby:db;create=true";
+	 
+	public static final String JDBC_URL = "jdbc:derby:D:\\db;create=true";
 	
 	private static Logger logger = LogManager.getLogger(DerbyConfig.class.getName());
-
+	
+	public static void main(String args[]){
+		System.out.println(OSProperties.getInstance().getUserHomeDirectory() + 
+						   OSProperties.getInstance().getOSFileSeparator() + 
+						   OSProperties.getInstance().getPrimaryFolderName() +
+						   OSProperties.getInstance().getOSFileSeparator() + 
+						   OSProperties.getInstance().getDatabaseFolderName());
+	}
 }
+
+
--- src/main/java/com/boxupp/utilities/OSProperties.java
+++ src/main/java/com/boxupp/utilities/OSProperties.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ *  Copyright 2014 Paxcel Technologies
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *******************************************************************************/
 package com.boxupp.utilities;
 
 public class OSProperties {
@@ -15,6 +30,7 @@ public class OSProperties {
 	private String manifestsDirName = "manifests";
 	private String nodeFileName = "site.pp";
 	private String dockerVagrantFileDir = "docker";
+	private String databaseFolderName = "db";
 			
 	private OSProperties(){
 			OSFileSeparator = System.getProperty("file.separator");
@@ -109,5 +125,12 @@ public class OSProperties {
 		this.dockerVagrantFileDir = dockerVagrantFileDir;
 	}
 
+	public String getDatabaseFolderName() {
+		return databaseFolderName;
+	}
+
+	public void setDatabaseFolderName(String databaseFolderName) {
+		this.databaseFolderName = databaseFolderName;
+	}
 	
 }
